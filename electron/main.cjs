483d3e0a42a0cb21d3b5abc3d10f800446dc801f
ShotaYoshida <dyqrks@gmail.com>
2025-11-18 17:27:51 +0900

    Ensure correct production loading and enforce shortcut creation in NSIS installer.

--- electron/main.cjs
+++ electron/main.cjs
@@ -1,4 +1,3 @@
-// electron/main.js
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
 
@@ -13,12 +12,11 @@ function createWindow() {
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
-    },
+    }
   });
 
   if (isDev) {
     win.loadURL("http://localhost:5173/");
-    win.webContents.openDevTools();
   } else {
     const indexPath = path.join(__dirname, "../dist/index.html");
     win.loadFile(indexPath);
@@ -27,16 +25,4 @@ function createWindow() {
 
 app.whenReady().then(() => {
   createWindow();
-
-  app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
-});
-
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
 });
